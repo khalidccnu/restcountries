8b5c0d032420c530df39a70d5c6464f023cf9821
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-03-22 10:02:05 +0600

    footer added

---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="mt-auto py-3 bg-light">
+            <div className="container">
+                <p className="mb-0 text-center">&copy; 2023 All Rights Reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
