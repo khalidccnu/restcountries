f43973f6301bfa6bd18a0e1b7cc2f6d20da82133
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-03-25 05:58:17 +0600

    show all country functionality added

--- src/component/Countries.jsx
+++ src/component/Countries.jsx
@@ -3,6 +3,8 @@ import Country from "./Country.jsx";
 
 function Countries({searchValue}) {
     const [country, setCountry] = useState([]);
+    const [ptCountry, setAllCountry] = useState([]);
+
     useEffect(_ => {
         let searchKeyword;
 
@@ -11,13 +13,28 @@ function Countries({searchValue}) {
 
         fetch("https://restcountries.com/v3.1/" + searchKeyword)
             .then(response => response.json())
-            .then(data => setCountry(data));
+            .then(data => {
+                setCountry(data);
+                setAllCountry(data.length > 9 ? data.slice(0, 9) : data);
+            });
     }, [searchValue]);
+
+    function showAllCountry({target: elem}) {
+        setAllCountry(country);
+        elem.parentElement.classList.add("d-none");
+    }
 
     return (
         <section className="py-3">
             <div className="container">
-                <div className="countries d-grid gap-3">{country.map((elem, index) => <Country key={index} country={elem} />)}</div>
+                <div className="countries d-grid gap-3">{ptCountry.map((elem, index) => <Country key={index} country={elem} />)}</div>
+                {
+                    country.length > 9 ? (
+                        <div className="mt-4 text-center">
+                            <button role="button" className="btn btn-sm btn-success" onClick={e => showAllCountry(e)}>Show All</button>
+                        </div>
+                    ) : ""
+                }
             </div>
         </section>
     )
